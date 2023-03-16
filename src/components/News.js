a3f5b940f558879b1035947bc5cbc588101709dd
Anilreddy2009 <116097932+Anilreddy2009@users.noreply.github.com>
2023-03-16 18:44:21 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -11,13 +11,14 @@ export const News = () => {
           }
           fetchData()
   },[])
+  console.log(dataAll)
   return (
     <div className='container my-3'>
       <h2>News-Top headlines</h2>
       <div className="row">
       {dataAll.map((element)=>{
         return <div className="col-md-4" key={element.url}>
-            <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,88)} urlToImage={element.urlToImage} url={element.url}/>
+            <NewsItem title={element.title.slice(0,20)} description={element.description.slice(0,40)} urlToImage={element.urlToImage} url={element.url}/>
         </div>
       })}
       </div>
